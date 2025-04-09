6b787b0d0d3f88c8de163b94d8128809051344b6
SakarSilwal <sakarsilwal123@gmail.com>
2025-04-09 23:37:00 +0545

    first commit in flutter

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(FlutterApp());
+}
+
+class FlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "FlutterApp",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: DashBoardScreen(),
+    );
+  }
+}
+
+class DashBoardScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dashboard", style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.black,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            color: Colors.white,
+            width: double.infinity,
+            height: double.infinity,
+            child: Center(
+              child: Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(21),
+                    bottomRight: Radius.circular(21),
+                  ),
+                  border: Border.all(width: 5, color: Colors.amber),
+                  boxShadow: [BoxShadow(blurRadius: 30, color: Colors.red)],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
